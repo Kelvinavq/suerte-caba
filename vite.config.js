2b0d7f36b8d4f6bf095fdc452c7cd94046b6c6b9
kelvinavq <kvalera200244@gmail.com>
2024-06-14 23:18:29 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Opcionalmente puedes usar '/dist/' dependiendo de cómo sirves tu aplicación
+  base: './dist/',
   build: {
     outDir: 'dist'
   }
